ad317905a948ccc50d3d6b223bbd1ba5d52aeb00
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-03-31 10:57:57 +0700

    Add project files.

---
+++ quicksort_156/quicksort_156.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//array of integres to hold values
+int arr[20];
+int cmp_count = 0; //numvber of comprasion
+int mov_count = 0; //number of movemnt
+int n;
+void input() {
+	while (true)
+	{
+		cout << "massukan panjang element array": ";
+			cin >> ;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\n maksimum panjang array adalah 20" << endl;
+	}
+}
